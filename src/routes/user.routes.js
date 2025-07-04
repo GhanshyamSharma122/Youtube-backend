452fc317acbf70a7a15f7ee4424148589cf74571
shyam poudel <poudelshyam844@gmail.com>
2025-07-04 17:32:21 +0530

    refactor: enhance user routes by adding account update and password change functionalities

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refershAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refershAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-const router=Router()
+const router = Router()
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser
@@ -19,7 +19,22 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refershAccessToken)
-
+router.route("/get-current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account-details").post(verifyJWT, updateAccountDetails)
+router.route("/update-user-avatar").post(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+)
+router.route("/update-user-cover-image").post(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+)
+router.route("/chagepassword").post(
+    verifyJWT,
+    changeCurrentPassword
+)
 export default router
