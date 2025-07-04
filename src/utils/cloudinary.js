d31518084b8a2c979de2c14e4025f645a4366218
shyam poudel <poudelshyam844@gmail.com>
2025-07-04 14:08:40 +0530

    implement user authentication with JWT, including login, logout, and token refresh functionality

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,8 @@ const uploadOnCloudinary=async (localFilePath)=>{
             resource_type:"auto"
         })
         //file has been uploaded sucessfully
-        console.log("the file is uploaded on cloudinary",response.url);
+        // console.log("the file is uploaded on cloudinary",response.url);
+        fs.unlinkSync(localFilePath)
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath) //remove the locally saved temporarily file as the upload operation got failed
