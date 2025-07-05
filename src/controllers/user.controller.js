156cff375e3d6738a4c0eb539c15efe0d07f533c
shyam poudel <poudelshyam844@gmail.com>
2025-07-05 14:03:02 +0530

    fix: correct error variable reference in asyncHandler and update dotenv path comment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -401,14 +401,14 @@ const getWatchHistory = asyncHandler(async (req, res) => {
         }
     ])
     return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            user[0].watchHistory,
-            "watch history fetched successfully"
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user[0].watchHistory,
+                "watch history fetched successfully"
+            )
         )
-    )
 })
 
 
